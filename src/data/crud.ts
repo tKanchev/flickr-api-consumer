b928243c6b086bea94dabd0442981ed22a394ec0
tKanchev <t.kanchev@yahoo.com>
2019-04-20 21:55:44 +0300

    Refactoring

--- src/data/crud.ts
+++ src/data/crud.ts
@@ -2,7 +2,7 @@ import fetchJsonp from 'fetch-jsonp';
 
 export function request() {    
     return async (url: string) => {       
-        const response = fetchJsonp('https://api.flickr.com/services/feeds/photos_public.gne?format=json', {
+        const response = fetchJsonp('url', {
             jsonpCallback: 'jsoncallback',
             timeout: 3000
           })
